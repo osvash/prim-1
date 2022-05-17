9cdbb7bb060f483134c4bc3b120feb90f11e45c4
willsh <66085223+osvash@users.noreply.github.com>
2022-05-17 18:34:45 +0000

    Product class was added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import {User} from './user.js'
+import {Product} from './product.js'
 
 const ryan = new User('ryan', 'ray')
 
 console.log(ryan)
+
+console.log( new Product('laptop' , 3000))
 
 
 
